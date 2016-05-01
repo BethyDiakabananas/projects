0e5c4f7d3ef2374a330e76082c1d1f489624edfe
Bethy Diakabana <bethyd123@gmail.com>
2016-05-01 00:45:18 -0400

    Delete fibonacci_recursive.py

--- Numbers/Fibonacci-Sequence/fibonacci_recursive.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(n):
-	""""Prints n numbers in a fibonacci sequence"""
-	while n > 0:
-		return fibonacci(n-1) + fibonacci(n-2)
-		n -= 1
-	return `
-
-n = int(raw_input("Enter a number for a fibonacci sequence: "))
-print fibonacci(n)
