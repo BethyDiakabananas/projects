10cff5b84c0dd27bf2f7f4d943c4ac190caffbd2
Bethy Diakabana <bethyd123@gmail.com>
2016-05-01 08:14:27 -0400

    Update fibonacci.py
    add fixed recursive implementation

--- Numbers/Fibonacci-Sequence/fibonacci.py
+++ Numbers/Fibonacci-Sequence/fibonacci.py
@@ -7,5 +7,17 @@ def fibonacci(n):
 		print int(((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5)))
 		n -= 1
 
+def fibonacci_recursive(n):
+	""""Prints n numbers in a fibonacci sequence"""
+	if n <= 1:
+		return n
+	else:
+		return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
 n = int(raw_input("Enter a number for a fibonacci sequence: "))
 fibonacci(n)
+
+n = int(raw_input("Enter a number for a fibonacci sequence done recursively: "))
+while n > 0:
+	print fibonacci_recursive(n)
+	n -= 1
