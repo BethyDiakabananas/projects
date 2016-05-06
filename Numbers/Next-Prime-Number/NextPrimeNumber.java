abae62a85a402a93f9e5c5d7aca28371d637ddb9
Bethy Diakabana <bethyd123@gmail.com>
2016-05-06 10:16:04 -0400

    Update NextPrimeNumber.java
    added edge case to check for one and two, and negative numbers. program should consider two a prime number, but not 1, nor n

--- Numbers/Next-Prime-Number/NextPrimeNumber.java
+++ Numbers/Next-Prime-Number/NextPrimeNumber.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class NextPrimeNumber {
 	private static boolean isPrime(int n) {
-		if (n % 2 == 0)
+		if (n == 2)
+			return true;
+		if (n <= 1 || n % 2 == 0)
 			return false;
 		for (int i = 3; i*i <= n; i+=2) {
 			if (n % i == 0) 
